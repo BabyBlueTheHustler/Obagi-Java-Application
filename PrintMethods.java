2e0d8a58dec7fed2109f35930b327c0db581034d
Ken Jr Johnson <159865042+BabyBlueTheHustler@users.noreply.github.com>
2025-03-18 15:47:27 +0100

    print method

---
+++ PrintMethods.java
@@ -0,0 +1,22 @@
+public class PrintMethods{
+	public static void main(String[] args){
+		//Using the print() Method
+		System.out.print("\"Learning Java is Fun\"\n");
+		System.out.print(" Hey come and learn Java with me");
+		
+		//using the println() 
+		System.out.println();
+		System.out.println("I want to tell you that Java is different from javascript");
+		
+		//Escape characters
+		System.out.println("\t This is the indented text");
+		
+		//Using the printf()
+		System.out.printf("My name is %s and i love %s %n", "John", "Java");
+		System.out.printf("I am living in No. %d Kaduna Street %n", 1);
+		System.out.printf(" I bought Java JDK with %c%.2f %n",'$', 159.345);
+		System.out.printf(" I bought Java JDK with %.1f Naira", 159.345);
+		
+		
+	}
+}
