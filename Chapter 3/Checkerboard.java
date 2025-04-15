452ce916c2e74862583e0134729a3ee157e468ec
Ken Jr Johnson <159865042+BabyBlueTheHustler@users.noreply.github.com>
2025-04-15 14:38:05 +0100

    Update Checkerboard.java

--- Chapter 3/Checkerboard.java
+++ Chapter 3/Checkerboard.java
@@ -1,7 +1,7 @@
 public class Checkerboard {
     public static void main(String[] args) {
         for (int i = 0; i < 8; i++) {
-            if (i % 2 != 0) System.out.print(" "); // Offset for alternate rows
+            if (i % 2 != 0) System.out.print(" ");
             for (int j = 0; j < 8; j++) {
                 System.out.print("* ");
             }
